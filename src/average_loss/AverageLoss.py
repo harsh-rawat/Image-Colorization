9eae1e57880ad0ef689aa739aecb72ff0b634130
Harsh Rawat <harsh.rawat@wisc.edu>
2020-04-17 21:30:30 -0500

    Made Average class generic

--- src/average_loss/AverageLoss.py
+++ src/average_loss/AverageLoss.py
@@ -4,20 +4,31 @@ import matplotlib.pyplot as plt
 
 
 class AverageLoss:
-    def __init__(self, base_path):
-        self.gen = Loss()
-        self.dis = Loss()
+    """
+        base_path :
+        This specifies the base path where losses and plots are saved!
+
+        models :
+        The format of this parameter is (No of models, List of names of all these models)
+    """
+
+    def __init__(self, base_path, models=(2, ['Generator', 'Discriminator'])):
+        self.all_models = [];
+        for i in range(models[0]):
+            self.all_models.append(Loss());
+
+        self.model_names = models[1]
         self.index = 0
         self.base_path = base_path
 
-    # Assuming order - gen, dis
+    # Assuming order - As passes to models[1]
     def add_loss(self, losses):
-        self.gen.add(losses[0])
-        self.dis.add(losses[1])
+        for i in range(len(self.all_models)):
+            self.all_models[i].add(losses[i])
 
-    def plot(self, iteration_factor=10):
-        plt.plot(self.gen.get_loss(), label='Generator')
-        plt.plot(self.dis.get_loss(), label='Discriminator')
+    def plot(self):
+        for i in range(len(self.all_models)):
+            plt.plot(self.all_models[i].get_loss(), label=self.model_names[i])
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.legend()
@@ -25,19 +36,23 @@ class AverageLoss:
         plt.show()
 
     def save(self, filename):
-        save_dict = {'gen': self.gen, 'dis': self.dis, 'index': self.index, 'base_path': self.base_path}
-        filepath = '{}/Loss_Checkpoints/{}_{}'.format(self.base_path, filename, self.index)
-        with open(filepath, 'wb') as file:
+        save_dict = {'index': self.index, 'base_path': self.base_path}
+        for i in range(len(self.all_models)):
+            save_dict[self.model_names[i]] = self.all_models[i]
+        save_dict['model_names'] = self.model_names;
+        file_path = '{}/{}_{}'.format(self.base_path, filename, self.index)
+        with open(file_path, 'wb') as file:
             pickle.dump(save_dict, file)
         print('Losses have been saved!')
         self.index += 1
 
     def load(self, filename, index):
-        filepath = '{}/Loss_Checkpoints/{}_{}'.format(self.base_path, filename, index)
+        filepath = '{}/{}_{}'.format(self.base_path, filename, index)
         with open(filepath, 'rb') as file:
             save_dict = pickle.load(file)
-            self.gen = save_dict['gen']
-            self.dis = save_dict['dis']
+            self.model_names = save_dict['model_names']
+            for i in range(len(self.all_models)):
+                self.all_models[i] = save_dict[self.model_names[i]]
             self.index = save_dict['index']
             self.base_path = save_dict['base_path']
-        print('Checkpoint has been restored!')
+        print('Loss checkpoint has been restored!')
